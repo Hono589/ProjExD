35c0321e6f6572c0aed01167de35433cc6c83613
maruyama <c0a21114b0@edu.teu.ac.jp>
2022-06-14 16:04:50 +0900

    quiz1

---
+++ ex01/Kadai1.py
@@ -0,0 +1,27 @@
+import random
+
+def main():
+  sei = shutudai()
+  kai(sei)
+
+def shutudai():
+  qu = [
+      {"q":"サザエの旦那の名前は？", "a":["マスオ", "ますお"]},
+      {"q":"カツオの妹の名前は？","a":["ワカメ", "わかめ"]},
+      {"q":"タラオはカツオから見てどんな関係？", "a": ["甥", "おい", "甥っ子", "おいっこ"]}
+      ]
+  print("問題：")
+  ra = random.randint(0, 2)
+  print(qu[ra]["q"])
+  return qu[ra]["a"]
+
+
+  def kaitou(sei):
+    a = input("答えてください")
+    if a in sei:
+      print("正解！！！")
+    else:
+      print("不正解！！！")
+
+  if __name__=="__main__":
+    main()

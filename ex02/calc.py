533798856a269c1c7563d2b6528a8395036c3f3f
maruyama <c0a21114b0@edu.teu.ac.jp>
2022-06-21 17:48:51 +0900

    変更3#10

--- ex02/calc.py
+++ ex02/calc.py
@@ -17,10 +17,7 @@ def button_click(event):
     entry.insert(tk.END, num)
 
   if num == "C":
-    a = entry.get()
-    s = eval(a)
     entry.delete(0, tk.END)
-    entry.insert(tk.END, s)
 
 #ボタンイベント
 '''def make_click(ch):
@@ -35,14 +32,14 @@ def button_click(event):
 if __name__ == "__main__":
   root = tk.Tk()
   root.title("電卓")
-  root.geometry("300x700")
+  #root.geometry("300x700")
 
   entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
   entry.grid(row=0, column=0, columnspan=80)
 
   r, c = 1, 0
   #numli = [9, 8, 7, 6, 5, 4, 3, 2, 1,0, "+"]
-  for i, num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1,0, "+", "-", "*", "/", "=", "C"]):
+  for i, num in enumerate([9, 8, 7, "+", 6, 5, 4, "-", 3, 2, 1, "=", 0, "*", "/", "C"]):
     btn = tk.Button(root,
                     text=f"{num}",
                     width=4,
@@ -59,7 +56,7 @@ if __name__ == "__main__":
     btn.bind("<1>",button_click)
     btn.grid(row=r, column=c, padx=5, pady=4)
     c += 1
-    if(i+1)%3 == 0:
+    if(i+1)%4 == 0:
         r += 1
         c = 0
   

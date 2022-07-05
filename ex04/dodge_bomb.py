8f60095fd9d54dae04b5dc40b5e3bdf760f3a06f
maruyama <c0a21114b0@edu.teu.ac.jp>
2022-07-05 15:46:20 +0900

    ren8

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -6,24 +6,24 @@ def main():
   clock = pg.time.Clock()
   pg.display.set_caption("逃げろ！こうかとん")
   screen_sfc = pg.display.set_mode((1600, 900)) #Surface
-  screen_rct = screen_sfc.get_rect()            #Rect
-  bgimg_sfc = pg.image.load("fig/pg_bg.jpg")    #Surface
+  screen_rct = screen_sfc.get_rect()            #画面用Rect
+  bgimg_sfc = pg.image.load("fig/pg_bg.jpg")    #背景画像用Surface
   bgimg_rct = bgimg_sfc.get_rect()              #Rect
   screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-  #練習3
-  kkimg_sfc = pg.image.load("fig/2.png")   #Surface
-  kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)  #Surface
-  kkimg_rct = kkimg_sfc.get_rect()
-  kkimg_rct.center = 900, 400
+  #練習3 こうかとん
+  kkimg_sfc = pg.image.load("fig/2.png")   #効果トン画像用Surface
+  kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)  #効果トン画像の拡大Surface
+  kkimg_rct = kkimg_sfc.get_rect()         #こうかとん画像用Rect
+  kkimg_rct.center = 900, 400              #こうかとんの中心を900、400に指定
 
   #練習5 爆弾
-  bmimg_sfc = pg.Surface((20, 20)) #Surface
-  bmimg_sfc.set_colorkey((0, 0, 0))
-  pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
-  bmimg_rct = bmimg_sfc.get_rect() #Rect
-  bmimg_rct.centerx = random.randint(0, screen_rct.width)
-  bmimg_rct.centery = random.randint(0, screen_rct.height)
+  bmimg_sfc = pg.Surface((20, 20)) #爆弾用Surface
+  bmimg_sfc.set_colorkey((0, 0, 0))   
+  pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)  #爆弾用Surfaceに円を描く。色・中心・半径を指定
+  bmimg_rct = bmimg_sfc.get_rect() #爆弾用Rect
+  bmimg_rct.centerx = random.randint(0, screen_rct.width)  #爆弾のx座標をランダムに決定
+  bmimg_rct.centery = random.randint(0, screen_rct.height)  #爆弾のy座標をランダムに決定
 
   vx, vy = +1, +1
 
@@ -59,10 +59,15 @@ def main():
 
     #練習5
     screen_sfc.blit(bmimg_sfc, bmimg_rct)
+    
     #練習7
     x, y = check_bound(bmimg_rct, screen_rct)
     vx *= x
     vy *= y
+
+    #練習8 爆弾の当たり判定
+    if kkimg_rct.colliderect(bmimg_rct) == True: #toriがbombと重なったらTrue
+            return #終了(mainから抜ける)
     
 
     pg.display.update()

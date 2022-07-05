dfaca7e06ac7aea45748e1d21a81b6cece9716ea
maruyama <c0a21114b0@edu.teu.ac.jp>
2022-07-05 14:35:40 +0900

    ren4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -10,21 +10,30 @@ def main():
   bgimg_rct = bgimg_sfc.get_rect()              #Rect
   screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
+  #練習3
   kkimg_sfc = pg.image.load("fig/2.png")   #Surface
   kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)  #Surface
   kkimg_rct = kkimg_sfc.get_rect()
   kkimg_rct.center = 900, 400
 
   #pg.display.update()
-  clock.tick(0.5)
+  #clock.tick(0.5)
 
   while True:
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
-    screen_sfc.blit(kkimg_sfc, kkimg_rct)
+    
 
     #練習2
     for event in pg.event.get():
       if event.type == pg.QUIT:return
+
+    #練習4
+    key_states = pg.key.get_pressed()#辞書
+    if key_states[pg.K_UP] == True: kkimg_rct.centery -= 1  #y座標を－1
+    if key_states[pg.K_DOWN] == True: kkimg_rct.centery += 1 #y +1
+    if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -= 1 #y -1
+    if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1 #y +1
+    screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
     pg.display.update()
     clock.tick(1000)
